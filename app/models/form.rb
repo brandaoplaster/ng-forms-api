12040d1fd0a4d3000131f687e8c627d76e925fb7
brandaoplaster <brandaoplaster@gmail.com>
2022-12-13 22:10:22 -0300

    chore: Adding validations in models

--- app/models/form.rb
+++ app/models/form.rb
@@ -5,4 +5,6 @@ class Form < ApplicationRecord
   belongs_to :user
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
+
+  validates :title, :description, :user, presence: true
 end
