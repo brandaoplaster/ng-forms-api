002bdb67117cbf47d92f163aca9a2dcd82c980dd
brandaoplaster <brandaoplaster@gmail.com>
2022-12-14 21:55:36 -0300

    feat: Adding action show to form

--- app/controllers/api/v1/forms_controller.rb
+++ app/controllers/api/v1/forms_controller.rb
@@ -1,11 +1,14 @@
 module Api::V1
   class FormsController < Api::V1::ApiController
+    before_action :set_form, only: [:show, :update, :destroy]
+
     def index
       @forms = current_api_v1_user.forms
       render json: @forms.to_json
     end
 
     def show
+      render json: @form, include: "questions"
     end
 
     def update
@@ -15,6 +18,12 @@ module Api::V1
     end
 
     def destroy
+    end
+
+    private
+
+    def set_form
+      @form = From.friendly.find(params[:id])
     end
   end
 end
