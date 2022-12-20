ccdb2c9625e3c90af3fb25005557a5fdd4f4003f
brandaoplaster <brandaoplaster@gmail.com>
2022-12-19 22:38:56 -0300

    feat: Adding update method to question controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,13 +1,32 @@
 module Api::V1
   class QuestionsController < Api::V1::ApiController
-    before_action :allow_only_owner, only: [:create]
+    before_action :set_form
+    before_action :allow_only_owner, only: [:create, :update]
+    before_action :set_question, only: [:update]
 
     def create
       @question = Question.create(question_params.merge(form: @form))
       render json: @question
     end
 
+    def update
+      @question.update(question_params)
+      render json: @question
+    end
+
     private
+
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
+    def set_form
+      @form = (@question) ? @question.form : Form.find(params[:form_id])
+    end
+
+    def question_params
+      params.require(:question).permit(:title, :kind)
+    end
 
     def allow_only_owner
       unless current_api_v1_user == @form.user
