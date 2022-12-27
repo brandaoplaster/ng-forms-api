ed26816297fe49f205280c9e18b6ae0599e80bf1
brandaoplaster <brandaoplaster@gmail.com>
2022-12-27 00:36:02 -0300

    test: Adding tests to action delete questions controller

--- spec/requests/api/v1/questions_spec.rb
+++ spec/requests/api/v1/questions_spec.rb
@@ -61,70 +61,6 @@ RSpec.describe "Api::V1::Questions", type: :request do
     end
   end
 
-  1
-  2
-  3
-  4
-  5
-  6
-  7
-  8
-  9
-  10
-  11
-  12
-  13
-  14
-  15
-  16
-  17
-  18
-  19
-  20
-  21
-  22
-  23
-  24
-  25
-  26
-  27
-  28
-  29
-  30
-  31
-  32
-  33
-  34
-  35
-  36
-  37
-  38
-  39
-  40
-  41
-  42
-  43
-  44
-  45
-  46
-  47
-  48
-  49
-  50
-  51
-  52
-  53
-  54
-  55
-  56
-  57
-  58
-  59
-  60
-  61
-  62
-  63
-  64
   describe "PUT /questions/:id" do
     context "With Invalid authentication headers" do
       it_behaves_like :deny_without_authorization, :put, "/api/v1/questions/0"
@@ -182,6 +118,54 @@ RSpec.describe "Api::V1::Questions", type: :request do
 
         it "returns 404" do
           delete "/api/v1/questions/0", params: { question: @question_attributes }, headers: header_with_authentication(@user)
+          expect_status(404)
+        end
+      end
+    end
+  end
+
+  describe "DELETE /questions/:id" do
+    before do
+      @user = create(:user)
+    end
+
+    context "With Invalid authentication headers" do
+      it_behaves_like :deny_without_authorization, :delete, "/api/v1/questions/0"
+    end
+
+    context "With valid authentication headers" do
+      context "When question exists" do
+        context "And user is the owner" do
+          before do
+            @form = create(:form, user: @user)
+            @question = create(:question, form: @form)
+            delete "/api/v1/questions/#{@question.id}", params: {}, headers: header_with_authentication(@user)
+          end
+
+          it "returns 200" do
+            expect_status(200)
+          end
+
+          it "question are deleted" do
+            expect(Question.all.count).to eql(0)
+          end
+        end
+
+        context "And user is not the owner" do
+          before do
+            @question = create(:question)
+            delete "/api/v1/questions/#{@question.id}", params: {}, headers: header_with_authentication(@user)
+          end
+
+          it "returns 403" do
+            expect_status(403)
+          end
+        end
+      end
+
+      context "When question dont exists" do
+        it "returns 404" do
+          delete "/api/v1/questions/0", params: {}, headers: header_with_authentication(@user)
           expect_status(404)
         end
       end
